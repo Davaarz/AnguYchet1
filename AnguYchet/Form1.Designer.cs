dfe0a600414f68e2cc3e0f8f3e6757d14d73dc5f
David Arzymanyan <davedaplusa@mail.ru>
2023-02-16 13:44:36 +0500

    Добавьте файлы проекта.

---
+++ AnguYchet/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace AnguYchet
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.button8 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button8
+			// 
+			this.button8.Location = new System.Drawing.Point(408, 248);
+			this.button8.Margin = new System.Windows.Forms.Padding(2);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(244, 55);
+			this.button8.TabIndex = 36;
+			this.button8.Text = "Выполненные заказы";
+			this.button8.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(526, 193);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(126, 58);
+			this.button1.TabIndex = 34;
+			this.button1.Text = "Заказанные изделия";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(408, 193);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(125, 58);
+			this.button2.TabIndex = 31;
+			this.button2.Text = "Изделия";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(135, 248);
+			this.button5.Margin = new System.Windows.Forms.Padding(2);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(244, 55);
+			this.button5.TabIndex = 35;
+			this.button5.Text = "Заказы";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(252, 193);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(127, 58);
+			this.button6.TabIndex = 33;
+			this.button6.Text = "Заказчики";
+			this.button6.UseVisualStyleBackColor = true;
+			// 
+			// button9
+			// 
+			this.button9.Location = new System.Drawing.Point(135, 193);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(123, 58);
+			this.button9.TabIndex = 32;
+			this.button9.Text = "Сотрудники";
+			this.button9.UseVisualStyleBackColor = true;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(650, 11);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(139, 106);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 30;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(249, 56);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(301, 21);
+			this.label1.TabIndex = 29;
+			this.label1.Text = "Учет заказов на предприятии УАПО";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
